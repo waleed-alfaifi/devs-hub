00f6ca34567e3eb3fd5ff284d6f90a4bbca6e557
Waleed <22619190+waleed-alfaifi@users.noreply.github.com>
2020-08-10 01:29:58 +0300

    initial home page layout

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  const topics = [
+    {
+      name: 'JavaScript',
+      imgUrl: '/images/icons/js.svg',
+    },
+    {
+      name: 'Python',
+      imgUrl: '/images/icons/python.svg',
+    },
+    {
+      name: 'PHP',
+      imgUrl: '/images/icons/php.svg',
+    },
+    {
+      name: 'Java',
+      imgUrl: '/images/icons/java.svg',
+    },
+    {
+      name: 'C#',
+      imgUrl: '/images/icons/c_sharp.svg',
+    },
+    {
+      name: 'TypeScript',
+      imgUrl: '/images/icons/typescript.png',
+    },
+    {
+      name: 'C++',
+      imgUrl: '/images/icons/c++.png',
+    },
+    {
+      name: 'Dart',
+      imgUrl: '/images/icons/dart.png',
+    },
+  ];
+
+  res.render('index', { title: 'Devs Hub', topics });
 });
 
 module.exports = router;
