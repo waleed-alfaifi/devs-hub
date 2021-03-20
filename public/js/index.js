fbba1d6ffb3be55db79b0147b29c59d82e052825
Waleed Alfaifi <22619190+waleed-alfaifi@users.noreply.github.com>
2021-03-20 16:30:24 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,7 @@ const createTopicButton = document.getElementById("create-topic-button");
 createTopicButton.addEventListener("click", () => {
   modal.classList.add("is-active");
 
-  document.querySelector(".modal-background").addEventListener("click", () => {
-    modal.classList.remove("is-active");
-  });
-
-  document.querySelector(".modal .delete").addEventListener("click", () => {
-    modal.classList.remove("is-active");
-  });
-
-  document.getElementById("modal-cancel").addEventListener("click", () => {
+  document.querySelector(".modal-background, .modal .delete, #modal-cancel").addEventListener("click", () => {
     modal.classList.remove("is-active");
   });
 });
