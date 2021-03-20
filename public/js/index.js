04924937f5c28925861cc46202c45ce2e99ca8ee
Waleed Alfaifi <22619190+waleed-alfaifi@users.noreply.github.com>
2021-03-20 16:37:10 +0300

    Revert index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,15 @@ const createTopicButton = document.getElementById("create-topic-button");
 createTopicButton.addEventListener("click", () => {
   modal.classList.add("is-active");
 
-  document.querySelectorAll(".modal-background, .modal .delete, #modal-cancel").addEventListener("click", () => {
+  document.querySelector(".modal-background").addEventListener("click", () => {
+    modal.classList.remove("is-active");
+  });
+
+  document.querySelector(".modal .delete").addEventListener("click", () => {
+    modal.classList.remove("is-active");
+  });
+
+  document.getElementById("modal-cancel").addEventListener("click", () => {
     modal.classList.remove("is-active");
   });
 });
